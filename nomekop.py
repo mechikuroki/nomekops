48535e8184e42ee148eb5bcd19610706e9af7d6b
mechikuroki <kurokimercedes@gmail.com>
2026-06-25 15:41:55 -0300

    sorts and transfers

--- nomekop.py
+++ nomekop.py
@@ -12,7 +12,6 @@ class LinkedList:
             self.next = None
 
     def append(self, data):
-        #aviso que si la posición dada es mayor a la cantidad de elementos esto lo agrega al final
         new_node = self.Node(data)
 
         if not self.head:
@@ -26,7 +25,7 @@ class LinkedList:
 
     def remove(self, target):
         if not self.head:
-            return
+            return False
         
         if self.head.data.name == target:
             x = self.head.data
@@ -45,6 +44,19 @@ class LinkedList:
                 return x
             current = current.next
         return False
+
+    def remove_first(self):
+        if not self.head:
+            return False
+        
+        x = self.head.data
+
+        if self.tail == self.head:
+            self.head = None
+            self.tail = None
+            return x
+        self.head = self.head.next
+        return x
 
     def size(self):
         count = 0
@@ -166,17 +178,18 @@ class Stack:
 #------------------------------------------------------------
 
 class Nomekop:
-    def __init__(self, name, damage, nomekop_id):
+    def __init__(self, name, damage, nomekop_id, nomekop_type):
         self.name = name
         self.damage = damage
         self.id = nomekop_id
+        self.type = nomekop_type
 
 def load_nomekops():
     nomekops = HashMap(15, 3)
     with open(Path('nomekops.jsonl').resolve(), 'r') as file:
         for i in file:
             obj = json.loads(i)
-            nomekops.append(obj["name"], Nomekop(obj["name"], obj["damage"], nomekops.hash(obj["name"])))
+            nomekops.append(obj["name"], Nomekop(obj["name"], obj["damage"], nomekops.hash(obj["name"]), obj["type"]))
         return nomekops
 
 def load_badges():
@@ -195,34 +208,90 @@ def load_captured():
         with open(Path('captured.jsonl').resolve(), 'r') as file:
             for idx, i in enumerate(file):
                 obj = json.loads(i)
-                captured.append(Nomekop(obj["name"], obj["damage"], nomekops.hash(obj["name"])))
+                captured.append(Nomekop(obj["name"], obj["damage"], nomekops.hash(obj["name"]), obj["type"]))
     return captured
 
-def add_to_captured(name, damage):
+def add_to_captured_list(x):
+    global captured
+    open(Path('captured.jsonl').resolve(), 'w').close()
+    with open(Path('captured.jsonl').resolve(), 'a') as file:
+        for i in x:
+            nomekop = {"name" : i.name, "damage" : i.damage, "type" : i.type}
+            file.write(json.dumps(nomekop) + '\n')
+    captured = load_captured()
+
+def add_to_captured(name, damage, ntype):
     global captured
     with open(Path('captured.jsonl').resolve(), 'a') as file:
-        nomekop = {"name" : name, "damage": damage}
+        nomekop = {"name" : name, "damage": damage, "type" : ntype}
         file.write(json.dumps(nomekop) + '\n')
     captured = load_captured()
+
 
 def remove_from_captured(name):
     global captured
     x = None
-    with open(Path('captured.jsonl').resolve, 'r') as infile, open('out.jsonl', "w") as outfile:
+    matched = False
+    with open(Path('captured.jsonl').resolve(), 'r') as infile, open('out.jsonl', "w") as outfile:
         for line in infile:
             if not line.strip():
                 continue
             
             obj = json.loads(line)
         
-            if obj.get("name") == name:
+            if obj.get("name") == name and matched == False:
                 x = obj
+                matched = True
                 continue  
             
             outfile.write(line)
 
     os.replace(Path('out.jsonl').resolve(), Path('captured.jsonl').resolve())
-    captured = load_captured(x['name'], x['damage'])
+    captured = load_captured()
+    return Nomekop(x['name'], x['damage'], nomekops.hash(x['name']), x["type"])
+
+def sort_by_name():
+    global captured
+    x = [captured.remove_first() for _ in range(captured.size())] 
+    n = len(x)
+    for i in range(n):
+        switched = False
+        for j in range(0, n - i - 1):
+            if x[j].name.lower() > x[j + 1].name.lower():
+                x[j], x[j + 1] = x[j + 1], x[j]
+                switched = True
+        if not switched:
+            break
+    add_to_captured_list(x)
+
+def sort_by_damage():
+    global captured
+    x = [captured.remove_first() for _ in range(captured.size())]
+    quicksort(x)
+    add_to_captured_list(x)
+
+
+def quicksort_part(arr, low, high):
+    pivot = arr[high].damage
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j].damage <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1
+
+
+def quicksort(arr, low=0, high=None):
+    if high is None:
+        high = len(arr) - 1
+
+    if low < high:
+        pivot_index = quicksort_part(arr, low, high)
+        quicksort(arr, low, pivot_index - 1)
+        quicksort(arr, pivot_index + 1, high)
 
 def display_team():
     global team
@@ -240,9 +309,19 @@ team = []
 koa = Stack()
 healing = Queue()
 
+last_heal_epoch = time.time()
 running = True
 while running:
-    print("---¡¡¡NOMEKOPS!!!---\n1. Ver Equipo Principal\n2. Ver Almacenamiento (PC)\n3. Capturar Nomekop\n4. Cambiar Equipo Principal\n5. Ordenar PC\n6. Transferir al Profesor Koa\n7. Deshacer Transferencia a Koa\n8. Ir al Centro Nomekop\n9. Salir")
+    print("---¡¡¡NOMEKOPS!!!---\n1. See Main Team\n2. See Storage\n3. Capture Nomekop\n4. Change Main Team\n5. Sort Storage\n6. Transfer to Professor Koa\n7. Undo Transfer to Koa\n8. Send Nomekop to Nomekop Center\n9. Fight Against a Gym Leader\n10. See Ekopdex\n11. See Earned Badges\n12. Exit")
+
+    if healing.size() > 0 and time.time() - last_heal_epoch > 60:
+        nomekop = healing.dequeue()
+        add_to_captured(nomekop.name, nomekop.damage, nomekop.type)
+        last_heal_epoch = time.time()
+        print()
+        print("Nomekop returned fron Nomekop Center!")
+        print()
+
     ans = input("What do you want to do?: ")
     match ans:
         case '1':
@@ -252,7 +331,7 @@ while running:
         case '3':
             nomekop = nomekops.find_random()
             print(f"You captured {nomekop.name}!")
-            add_to_captured(nomekop.name, nomekop.damage)
+            add_to_captured(nomekop.name, nomekop.damage, nomekop.type)
         case '4':
             display_team()
             captured.display()
@@ -261,37 +340,66 @@ while running:
             if team_full:
                 in_team = input("Who do you want to remove? (answer with the Nomekop's position in the list): ")
             try:
-                in_captured = captured.remove(in_captured)
+                in_captured = remove_from_captured(in_captured)
                 if in_captured == False:
                     raise Exception
                 if team_full:
-                    add_to_captured(team.pop(in_team))
+                    in_team = team.pop(in_team)
+                    add_to_captured(in_team.name, in_team.damage, in_team.type)
                 team.append(in_captured)
             except:
                 print("Error. Try again.")
 
         case '5':
-            pass
+            captured.display()
+            sort = input("Do you want to sort by name (1), type (2) or damage (3)?")
+            match sort:
+                case '1':
+                    sort_by_name()
+                case '2':
+                    pass
+                case '3':
+                    sort_by_damage()
+                case _:
+                    print("Invalid input. Try again.")
+            
         case '6':
             captured.display()
-            name = input("Who do you want to transfer to Professor Koa? (answer with the Nomekop's name): ")
+            nomekop = input("Who do you want to transfer to Professor Koa? (answer with the Nomekop's name): ")
             try:
-                name = captured.remove(name)
-                if name == False:
+                nomekop = remove_from_captured(nomekop)
+                if nomekop == False or koa.size() > 5:
                     raise Exception
-                koa.push(name)
+                koa.push(nomekop)
             except:
                 print("Error. Try again.")
 
         case '7':
             if koa.size() != 0:
-                add_to_captured(koa.pop())
+                x = koa.pop()
+                add_to_captured(x.name, x.damage, x.type)
                 print("Returned nomekop!")
             else:
                 print("No nomekops to return.")
         case '8':
-            pass
+            captured.display()
+            nomekop = input("Who do you want to transfer to the Nomekop Center? (answer with the Nomekop's name): ")
+            try:
+                nomekop = remove_from_captured(nomekop)
+                print(nomekop)
+                if nomekop == False:
+                    raise Exception
+                healing.enqueue(nomekop)
+                last_heal_epoch = time.time()
+            except:
+                print("Error. Try again.")
         case '9':
+            pass
+        case '10':
+            pass
+        case '11':
+            pass
+        case '12':
             running = False
         case _:
             print("Invalid answer. Try again.")
